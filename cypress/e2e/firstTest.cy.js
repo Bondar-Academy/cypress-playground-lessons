7e303aee294a4546a49846d4059d83c9646e2162
Artem Bondar <artem.bondar@gmail.com>
2025-07-24 18:38:32 -0500

    cypress locator methods

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -34,3 +34,18 @@ it('Hello world 1', () => {
     cy.get('[data-cy="inputEmail1"]')
 
 })
+
+it.only('Cypress Locator Methods', () => {
+    //Theory
+    //get() - to find elments on the page globally
+    //find() - to find only child elements
+    //contains() - to find web elements by text
+
+    cy.contains('Sign in')
+    cy.contains('[status="warning"]', 'Sign in')
+    cy.contains('nb-card', 'Horizontal form').find('button')
+    cy.contains('nb-card', 'Horizontal form').contains('Sign in')
+    cy.contains('nb-card', 'Horizontal form').get('button')
+
+
+})
