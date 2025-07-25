990df2582e02b983d124b12dbf32d338fcd19e1a
Artem Bondar <artem.bondar@gmail.com>
2025-07-25 15:29:12 -0500

    parent elements

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -60,5 +60,10 @@ it('Child Elements', () => {
 })
 
 it.only('Parent Elements', () => {
-    
+
+    cy.get('#inputEmail1').parents('form').find('button')
+
+    cy.contains('Using the Grid').parent().find('button')
+
+    cy.get('#inputEmail1').parentsUntil('nb-card-body').find('button')
 })
