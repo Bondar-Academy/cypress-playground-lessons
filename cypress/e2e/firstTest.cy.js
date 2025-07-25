ce2d7fffd4d36d5cde4509c5280c7f8220371961
Artem Bondar <artem.bondar@gmail.com>
2025-07-25 17:16:19 -0500

    cypress chains

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -69,5 +69,14 @@ it('Parent Elements', () => {
 })
 
 it.only('Cypress Chains', () => {
+    cy.get('#inputEmail1')
+        .parents('form')
+        .find('button')
+        .click()
 
+    cy.get('#inputEmail1')
+        .parents('form')
+        .find('nb-radio')
+        .first()
+        .should('have.text', 'Option 1')
 })
