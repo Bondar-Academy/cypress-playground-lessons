e02799deb18b7e036f481afe6bd0da6ccb47a9be
Artem Bondar <artem.bondar@gmail.com>
2025-07-26 16:35:25 -0500

    assertions

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -135,4 +135,15 @@ it('Extracting Values', () => {
 
 it.only('Assertions', () => {
 
+    cy.get('[for="exampleInputEmail1"]').should('have.text', 'Email address')
+
+    cy.get('[for="exampleInputEmail1"]').then( label => {
+        expect(label).to.have.text('Email address')
+    })
+
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then( emailLabel => {
+        expect(emailLabel).to.equal('Email address')
+        cy.wrap(emailLabel).should('equal', 'Email address')
+    })
+
 })
