37b16f30fd49424e2883c762e38271f02046705b
Artem Bondar <artem.bondar@gmail.com>
2025-07-26 17:41:10 -0500

    timeouts

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -149,5 +149,10 @@ it('Assertions', () => {
 })
 
 it.only('Timeouts', () => {
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Dialog').click()
 
+    cy.contains('Open with delay 10 seconds').click()
+    cy.get('nb-dialog-container nb-card-header', {timeout: 11000})
+        .should('have.text', 'Friendly reminder')
 })
