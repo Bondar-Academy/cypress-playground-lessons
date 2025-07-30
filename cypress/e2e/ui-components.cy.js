65dc48e10005af320c50bed568a728ee117b2d88
Artem Bondar <artem.bondar@gmail.com>
2025-07-30 16:27:05 -0500

    checkboxes

--- cypress/e2e/ui-components.cy.js
+++ cypress/e2e/ui-components.cy.js
@@ -40,4 +40,7 @@ it.only('checkboxes', () => {
     cy.contains('Modal & Overlays').click()
     cy.contains('Toastr').click()
 
+    cy.get('[type="checkbox"]').check({force: true})
+    cy.get('[type="checkbox"]').should('be.checked')
+
 })
