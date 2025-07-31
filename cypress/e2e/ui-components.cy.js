17d664457879b9687b13e4006bdd900eb5fb2653
Artem Bondar <artem.bondar@gmail.com>
2025-07-31 17:49:45 -0500

    dialog boxes

--- cypress/e2e/ui-components.cy.js
+++ cypress/e2e/ui-components.cy.js
@@ -73,9 +73,20 @@ it('tooltips', () => {
     cy.get('nb-tooltip').should('have.text', 'This is a tooltip')
 })
 
-it.only('dialog Boxes', () => {
+it.only('dialog boxes', () => {
     cy.contains('Tables & Data').click()
     cy.contains('Smart Table').click()
 
-    
+    //1.
+    cy.get('.nb-trash').first().click()
+    cy.on('window:confirm', confirm => {
+        expect(confirm).to.equal('Are you sure you want to delete?')
+    })
+
+    //2.
+    cy.window().then( win => {
+        cy.stub(win, 'confirm').as('dialogBox').returns(false)
+    })
+    cy.get('.nb-trash').first().click()
+    cy.get('@dialogBox').should('be.calledWith', 'Are you sure you want to delete?')
 })
