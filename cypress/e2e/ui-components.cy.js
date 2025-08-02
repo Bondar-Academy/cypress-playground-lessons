3fe74052ad4e2bddd5093cb5e5267ecd7e558cb5
Artem Bondar <artem.bondar@gmail.com>
2025-08-02 15:44:32 -0500

    sliders

--- cypress/e2e/ui-components.cy.js
+++ cypress/e2e/ui-components.cy.js
@@ -164,6 +164,11 @@ it('datepickers', () => {
 })
 
 it.only('sliders', () => {
-    
+
+    cy.get('[tabtitle="Temperature"] circle')
+        .invoke('attr', 'cx', '38.66')
+        .invoke('attr', 'cy', '57.75')
+        .click()
+    cy.get('[class="value temperature h1"]').should('contain.text', '18')
 
 })
