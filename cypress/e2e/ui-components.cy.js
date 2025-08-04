12cb67b31630e744d2e080985132f2c6e2f3da2d
Artem Bondar <artem.bondar@gmail.com>
2025-08-04 16:11:03 -0500

    ifames

--- cypress/e2e/ui-components.cy.js
+++ cypress/e2e/ui-components.cy.js
@@ -183,6 +183,17 @@ it('drag and drop', () => {
 it.only('iframes', () => {
     cy.contains('Modal & Overlays').click()
     cy.contains('Dialog').click()
+    cy.frameLoaded('[data-cy="esc-close-iframe"]')
+
+    cy.iframe('[data-cy="esc-close-iframe"]').contains('Open Dialog with esc close').click()
+    cy.contains('Dismiss Dialog').click()
+
+    cy.enter('[data-cy="esc-close-iframe"]').then( getBody => {
+        getBody().contains('Open Dialog with esc close').click()
+        cy.contains('Dismiss Dialog').click()
+        getBody().contains('Open Dialog without esc close').click()
+        cy.contains('OK').click()
+    })
 
 
 })
