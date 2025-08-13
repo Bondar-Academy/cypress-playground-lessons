88a06319d5d3251359bd75b531299f521b8f4c80
Artem Bondar <artem.bondar@gmail.com>
2025-08-13 17:03:38 -0500

    parametrized page objects

--- cypress/e2e/pageObjects.cy.js
+++ cypress/e2e/pageObjects.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import { onDatepickerPage } from "../page-objects/datepickerPage"
+import { onFormLyoutsPage } from "../page-objects/formLayoutsPage"
 import { navigateTo } from "../page-objects/navigationPage"
 
 beforeEach('Open application', () => {
@@ -11,4 +13,13 @@ it('navigation test', () => {
     navigateTo.datePickerPage()
     navigateTo.tooltipPage()
     navigateTo.toastrPage()
+})
+
+it.only('test with page object', () => {
+    navigateTo.formLayoutsPage()
+    onFormLyoutsPage.submitUsingTheGridForm('test@test.com', 'Welcome', 1)
+    onFormLyoutsPage.submitBasicForm('artem@test.com', 'Welcome', false)
+    navigateTo.datePickerPage()
+    onDatepickerPage.selectCommonDatepickerDateFromToday(5)
+    onDatepickerPage.selectRangePickerDateFromToday(10, 50)
 })
