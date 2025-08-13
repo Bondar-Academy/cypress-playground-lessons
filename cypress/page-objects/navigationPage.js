0c1e38cea80f132f6582c8d9a9d39f7c2d2d71a4
Artem Bondar <artem.bondar@gmail.com>
2025-08-13 14:14:18 -0500

    conditional function

--- cypress/page-objects/navigationPage.js
+++ cypress/page-objects/navigationPage.js
@@ -1,25 +1,32 @@
 
+function selectGroupMenuItem(groupItemName){
+    cy.contains('a', groupItemName).invoke('attr', 'aria-expanded').then( attr => {
+        if(attr.includes('false')){
+            cy.contains('a', groupItemName).click()
+        }
+    })
+}
+
 
 class NavigationPage {
 
     formLayoutsPage() {
-        cy.contains('Forms').click()
+        selectGroupMenuItem('Forms')
         cy.contains('Form Layouts').click()
     }
 
     datePickerPage() {
-        cy.contains('Forms').click()
-        cy.wait(500)
+        selectGroupMenuItem('Forms')
         cy.contains('Datepicker').click()
     }
 
     toastrPage() {
-        cy.contains('Modal & Overlays').click()
+        selectGroupMenuItem('Modal & Overlays')
         cy.contains('Toastr').click()
     }
 
     tooltipPage() {
-        cy.contains('Modal & Overlays').click()
+        selectGroupMenuItem('Modal & Overlays')
         cy.contains('Tooltip').click()
     }
 
